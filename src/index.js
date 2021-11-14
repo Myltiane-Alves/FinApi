9e1bef6cab1aa959598bdf40f5f31fa51aa2ac3f
Myltiane Alves <myltiane.aux@gmail.com>
2021-11-14 14:09:04 -0300

    middlewares

--- src/index.js
+++ src/index.js
@@ -3,6 +3,22 @@ const { v4: uuidv4 } = require("uuid")
 const app = express();
 
 const customers = [];
+
+// Middleware
+function verifyIfExistsAccountCPF(request, response, next) {
+    const { cpf } = request.headers;
+
+    const customer = customers.find(customer => customer.cpf === cpf);
+
+    if (!customer) {
+        return response.status(400).json({ error:  "Customer not found"});
+    }
+
+    request.customer = customer;
+
+    return next();
+}
+
 app.use(express.json());
 
 
@@ -27,10 +43,8 @@ app.post("/account", (request, response) => {
     return response.status(201).send()
 });
 
-app.get("/statement/:cpf", (request, response) => {
-    const { cpf } = request.params;
-
-    const customer = customers.find(customer => customer.cpf === cpf);
+app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
+    const { customer } = request;
 
     return response.json(customer.statement);
 })
